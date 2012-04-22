6c2e6f4c70844c3f4dad4b8429f53d9b19ab40cd
Ben Cronin <ben@pusherhq.com>
2012-04-22 16:49:10 -0700

    Avoid error on createdatabase when user database does not exist

--- base/lib/Naan/App.php
+++ base/lib/Naan/App.php
@@ -2,26 +2,31 @@
 
 function app_create_database()
 {
-	$db = naan_db_connect();
-	
-	naan_db_create_table($db, "todo_list", "id", array(
-		'id'		=>	'INT AUTO_INCREMENT',
-		'user_id'	=> 	'INT',
-		'title'		=>  'TEXT'
-	));
-	
-	naan_db_create_table($db, "users", "id", array(
-		'id'			=>	'INT AUTO_INCREMENT',
-		'first_name'	=>	'TEXT',
-		'last_name'		=>	'TEXT',
-	));
-	
-	$db = null;
+    $db = naan_db_connect();
+    
+    naan_db_create_table($db, "todo_list", "id", array(
+        'id'        =>  'INT AUTO_INCREMENT',
+        'user_id'   =>  'INT',
+        'title'	    =>  'TEXT'
+    ));
+    
+    naan_db_create_table($db, "users", "id", array(
+        'id'            =>	'INT AUTO_INCREMENT',
+        'first_name'    =>	'TEXT',
+        'last_name'     =>	'TEXT',
+    ));
+    
+    $db = null;
 }
 
 function app_get_current_user()
 {
-	$user_id = naan_current_user_id();
-	return naan_db_get_row("users", array('id' => $user_id));
+    $user_id = naan_current_user_id();
+    try {
+        return naan_db_get_row("users", array('id' => $user_id));
+    } catch (Exception $e)
+    {
+        return NULL;
+    }
 }
 
