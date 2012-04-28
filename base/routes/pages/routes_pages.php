e03708a7485603af304025b6c53e11d09fb0582e
Ben Cronin <ben@pusherhq.com>
2012-04-28 13:22:28 -0700

    More additions to the framework

--- base/routes/pages/routes_pages.php
+++ base/routes/pages/routes_pages.php
@@ -4,10 +4,10 @@
 // Routes table
 //===========================================================================//
 
-$app->get('/',                  'routes_get_landing');
-$app->get('/home',              'routes_get_home');
-$app->get('/architecture',      'routes_get_architecture');
-$app->get('/tools/tasks',       'routes_get_tools_tasks');
+$app->get('/',                              'routes_get_landing');
+$app->get('/home',                          'routes_get_home');
+$app->get('/tools/tasks',                   'routes_get_tools_tasks');
+$app->get('/documentation/:page_name',      'routes_get_documentation');
 
 //===========================================================================//
 // Routing functions
@@ -28,15 +28,21 @@ function routes_get_home()
     return $app->render('home.html', $data);
 }
 
-function routes_get_architecture()
-{
-    global $app;
-    return $app->render('architecture.html'); 
-}
-
 function routes_get_tools_tasks()
 {
     global $app;
     return $app->render('tasks.html'); 
+}
+
+/*
+    This creates a generic page servering directory.  This avoids needing to
+    create a simple page render function for every single page in the
+    directory, but still maintains an overall strategy of requiring explicit
+    route names for every page on the site.
+ */
+function routes_get_documentation($page_name)
+{
+    global $app;
+    return $app->render("documentation/$page_name.html");
 }
 
